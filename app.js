49df45bbb80c9efdf2b75a495aab951fbe09ae0f
JChaparroG <jesuscha0706@gmail.com>
2025-01-21 16:36:47 -0500

    Versión final juego del amigo secreto

---
+++ app.js
@@ -0,0 +1,67 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    // Capturar el valor del campo de entrada ingresado por el usuario
+    let nombreDeAmigo = document.getElementById('amigo').value.trim();
+
+    /* Realizamos esta validación para asegurarnos de que el 
+    // campo no esté vacío.
+    //  Si está vacío, mostramos un alert con un mensaje de error
+    */
+    if (nombreDeAmigo === '') {
+        alert('Por favor, inserta un nombre');
+        return;
+    }
+    amigos.push(nombreDeAmigo);
+    limpiarCampo();
+    mostrarLista();
+  
+}
+
+// Función que permite limpiar el campo de entrada al ingresar el nombre de amigo
+function limpiarCampo() {
+    document.getElementById('amigo').value = '';
+}
+
+function mostrarLista() {
+    // Obtener el elemento de la lista
+    let listaDeAmigos = document.getElementById('listaAmigos');
+    // Limpiar la lista existente
+    listaDeAmigos.innerHTML = "";
+
+    // Iterar sobre el arreglo de amigos
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaDeAmigos.appendChild(li);
+    });
+    return;
+}
+
+function sortearAmigo() {
+    // Validamos si hay amigos en la lista
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    // Generamos un índice aleatorio
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    // Aquí obtenemos el nombre del amigo sorteado
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    // Por ultimo mostramos el resultado en el elemento con el ID 'resultado'
+    const resultado = document.getElementById('resultado');
+    resultado.textContent = `El amigo secreto es: ${amigoSorteado}`;
+}
+
+// Llamamos a mostrarLista inicialmente para mostrar la lista vacía
+mostrarLista();
+
+
+
+
+
